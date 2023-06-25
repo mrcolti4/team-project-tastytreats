1c9e83493ae2a1220da2b556d996061f4ee8cf17
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-06-25 23:36:26 +0200

    fix bag  css

---
+++ src/js/service/filter/clear_input.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('js-input');
+const clinInput = document.querySelector('js-clin-input');
+const serchIcon = document.querySelector('js-icon-serch');
+function workInputStyle(input, clinInput) {
+  if (!input.value === '') {
+    console.log(pizda);
+    clinInput.classList.replace(svg - serch, svg - serch - active);
+  }
+}
