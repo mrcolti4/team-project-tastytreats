e6910882b21347416ebfafbe09f3973dc48333f3
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-06-26 23:22:45 +0300

    fix bags MSH

--- src/js/service/filter/clear_input.js
+++ src/js/service/filter/clear_input.js
@@ -1,14 +1,29 @@
-// console.log('helow world');
+
 const input = document.querySelector('.js-input');
 const clinInput = document.querySelector('.js-clin-input');
 const serchIcon = document.querySelector('.js-icon-serch');
-// console.log(input.value);
-input.addEventListener('input', workInputStyle);
-function workInputStyle() {
-  console.log(input.value.trim().length);
-  if (!input.value.trim().length === 0) {
+
+
+input.addEventListener('focus', () => {
+  serchIcon.classList.add('active');
+});
+input.addEventListener('blur', () => {
+  serchIcon.classList.remove('active');
+});
+
+input.addEventListener('input', turnOnOffCross);
+clinInput.addEventListener('click', clickCross);
+
+function turnOnOffCross() {
+  console.log(input.value.trim().length > 0);
+  if (input.value.trim().length > 0) {
     clinInput.classList.add('active');
-    clinInput.serchIcon.replace('svg-serch-clous', 'svg-serch-clous-active');
+    return;
   }
+  clinInput.classList.remove('active');
 }
-input.addEventListener('input', workInputStyle);
+
+function clickCross() {
+  input.value = '';
+}
+
