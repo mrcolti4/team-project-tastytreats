820b8a7eec42fc6d50511a0f84bf23012dad8bb0
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-06-26 21:37:23 +0200

    comit
    gs

--- src/js/service/filter/clear_input.js
+++ src/js/service/filter/clear_input.js
@@ -1,9 +1,14 @@
-const input = document.querySelector('js-input');
-const clinInput = document.querySelector('js-clin-input');
-const serchIcon = document.querySelector('js-icon-serch');
-function workInputStyle(input, clinInput) {
-  if (!input.value === '') {
-    console.log(pizda);
-    clinInput.classList.replace(svg - serch, svg - serch - active);
+// console.log('helow world');
+const input = document.querySelector('.js-input');
+const clinInput = document.querySelector('.js-clin-input');
+const serchIcon = document.querySelector('.js-icon-serch');
+// console.log(input.value);
+input.addEventListener('input', workInputStyle);
+function workInputStyle() {
+  console.log(input.value.trim().length);
+  if (!input.value.trim().length === 0) {
+    clinInput.classList.add('active');
+    clinInput.serchIcon.replace('svg-serch-clous', 'svg-serch-clous-active');
   }
 }
+input.addEventListener('input', workInputStyle);
