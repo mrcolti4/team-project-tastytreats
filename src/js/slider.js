89fdf96bffc91ece015a9fc4f0cc24787a4b74f9
dmihnenko <110346306+dmihnenko@users.noreply.github.com>
2023-06-28 02:52:13 +0300

    hero update

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,39 +1,43 @@
-let $ = require( "jquery" );
+let $ = require('jquery');
 
 // Налаштування слайдеру
-$(document).ready(function(){
-    $('.slider').slick({
-        arrows: false,
-        dots: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        autoplay: true,
-        autoplaySpeed: 6000,
-
-    })
-})
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 6000,
+  });
+});
 // Пошук елемента DOM
 const refs = {
-    slider: document.querySelector('.slider'),
-   }
+  slider: document.querySelector('.slider'),
+};
 
 let arrEvents = null;
 // Отримуемо інформацію з серверу
 
 async function fetchEvents() {
-    let response = await fetch('https://tasty-treats-backend.p.goit.global/api/events');
-    let arrEvents = await response.json();
-   
-    return arrEvents;
+  let response = await fetch(
+    'https://tasty-treats-backend.p.goit.global/api/events'
+  );
+  let arrEvents = await response.json();
+
+  return arrEvents;
 }
-fetchEvents()
-// Робимо розмітку 
+
+// Тестую запит
+fetchEvents();
+// Робимо розмітку
 
 function renderSlider(arrEvents) {
-    console.log(arrEvents)
-   
-    const markup = arrEvents.map((event) => {
-        return `
+  console.log(arrEvents);
+
+  const markup = arrEvents
+    .map(event => {
+      return `
 <div class="slider-cook" data-id="${event._id}>
         <div class="slider-cook" >
         <img src="${event.cook.imgUrl}" alt="${event.cook.name}">
@@ -52,21 +56,16 @@ function renderSlider(arrEvents) {
       </div>
        
         `;
-    }).join('');
+    })
+    .join('');
 
-    refs.slider.insertAdjacentHTML('beforeend', markup);
-  
-  
+  refs.slider.insertAdjacentHTML('beforeend', markup);
 }
 
-
-
-
-
-// }
+// Виконую запит, та роблю розмітку.
 
 async function fetchAndRender() {
-    const data = await fetchEvents();
-    renderSlider(data);
+  const data = await fetchEvents();
+  renderSlider(data);
 }
-fetchAndRender()
+// fetchAndRender()
