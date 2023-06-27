d33040af1d94f392374a6b39d1e3408a5447e8ce
mrcolti4 <kbulygin456@gmail.com>
2023-06-27 16:49:47 +0300

    Create all recipes block

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './js/slider';
 import './js/all-categories';
+import './js/ratings';
 import './js/popular';
-import './js/service/pop-up'
+import './js/service/pop-up';
 import './js/popular';
-import './js/pagination'
+import './js/pagination';
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,10 +1,7 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
-console.log('ACCEPT');
-console.log('oooo');
 const container = document.getElementById('tui-pagination-container');
-console.log(container);
 
 const options = {
   totalItems: 10,
@@ -16,20 +13,21 @@ const options = {
   lastItemClassName: 'tui-last-child',
   template: {
     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
       '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</a>',
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-      '</a>'
-  }
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
 };
 
 const paginationP = new Pagination(container, options);
