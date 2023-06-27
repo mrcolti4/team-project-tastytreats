d165f19bd5a1d14cca23d82e1256438ef233d1e3
mrcolti4 <kbulygin456@gmail.com>
2023-06-27 22:59:30 +0300

    Add all recipes script

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import './js/header-script';
 
 import './js/header-mobile-menu';
 
-import './js/all-categories';
 import './js/ratings';
+import './js/all-categories';
+import './js/all-recipes';
 import './js/popular';
 import './js/service/pop-up';
-import './js/popular';
 import './js/pagination';
