c564f5eaefac2a87b6a99d1e0a112aebd281560c
OksanaSemenovska <oksanakononchuk1@ukr.net>
2023-06-27 18:39:42 +0300

    scroll and open/close modal

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,19 +1,35 @@
 
-/** Відкриття та закриття модального вікна.*/
-$(document).ready(function () {
-  $(".modal-close-btn").each(function () {
-    $(this).click(function () {
-      $(".modal").css("display", "none");
-       $(".backdrop").css("visibility", "hidden");
-    });
-  });
-  $(".btn").each(function () {
-    $(this).click(function () {
+// /** Відкриття та закриття модального вікна з конкретною назвою p.*/
+
+ $(document).ready(function() {
+    $(".btn-popup").click(function() {
+      // Отримуємо текст з елементу, на який натиснули
+      var title = $(this).text();
       $(".modal").css("display", "block");
       $(".backdrop").css("visibility", "visible");
+      $("#modal-title").text(title);
+    });
+    $(".modal-close-btn").click(function() {
+      $(".modal").css("display", "none");
+      $(".backdrop").css("visibility", "hidden");
     });
   });
-});
+
+// $(document).ready(function () {
+//   $(".modal-close-btn").each(function () {
+
+//     $(this).click(function () {
+//       $(".modal").css("display", "none");
+//       $(".backdrop").css("visibility", "hidden");
+//     });
+//   });
+//   $(".btn-popup").each(function () {
+//     $(this).click(function () {
+//       $(".modal").css("display", "block");
+//       $(".backdrop").css("visibility", "visible");
+//     });
+//   });
+// });
 
 
 /** CКРОЛ */
@@ -21,23 +37,27 @@ $(document).ready(function () {
 const recipeElements = document.getElementsByClassName('recipes-container');
 
 Array.from(recipeElements).forEach((element) => {
-  const elementHeight = element.scrollHeight;
+  const elementHeightCook = element.scrollHeight;
 
-  if (elementHeight > element.offsetHeight) {
+  if (elementHeightCook > element.offsetHeight) {
     element.style.overflowY = 'scroll';
     element.style.scrollbarWidth = 'thin'; 
     element.style.msOverflowStyle = 'scrollbar'; 
+   
   }
 });
 
 const recipeCook = document.getElementsByClassName('rcooking-recipes');
 
 Array.from(recipeCook).forEach((element) => {
-  const elementHeight = element.scrollHeight;
+  const elementHeightRec = element.scrollHeight;
 
-  if (elementHeight > element.offsetHeight) {
+  if (elementHeightRec > element.offsetHeight) {
     element.style.overflowY = 'scroll';
     element.style.scrollbarWidth = 'thin'; 
     element.style.msOverflowStyle = 'scrollbar'; 
+    
   }
 });
+
+
