8c90b6cfe99a08d96b1a144026fec031188dc0ba
OksanaSemenovska <oksanakononchuk1@ukr.net>
2023-06-29 00:13:22 +0300

    scrollUP#2
    change

--- src/js/btn-up.js
+++ src/js/btn-up.js
@@ -1,18 +1,52 @@
-  window.addEventListener('scroll', function() { scrollFunction() });
-            function scrollFunction() {
-            let scrollButton = document.querySelector('.scroll-icon');
-            if (window.pageYOffset > 200) {
-                scrollButton.style.display = "block";
-            } else {
-                scrollButton.style.display = "none";
-            }
-        }
-        function scrollToTop() {
-            var currentPosition = window.pageYOffset;
-            if (currentPosition > 0) {
-                window.requestAnimationFrame(scrollToTop);
-                window.scrollTo(0, currentPosition - currentPosition / 10);
-            }
-        }
-        let svgIcon = document.querySelector('.scroll-btn-icon');
-        svgIcon.addEventListener('click', scrollToTop);
+export const btnUp = {
+  el: document.querySelector('.scroll-icon'),
+    scrolling: false,
+    show() {
+    if (this.el.classList.contains('scroll-icon__hide') && !this.el.classList.contains('scroll-icon__hiding')) {
+      this.el.classList.remove('scroll-icon__hide');
+      this.el.classList.add('scroll-icon__hiding');
+      window.setTimeout(() => {
+        this.el.classList.remove('scroll-icon__hiding');
+      }, 300);
+    }
+  },
+  hide() {
+    if (
+      !this.el.classList.contains('scroll-icon__hide') &&
+      !this.el.classList.contains('scroll-icon__hiding')
+    ) {
+      this.el.classList.add('scroll-icon__hiding');
+      window.setTimeout(() => {
+        this.el.classList.add('scroll-icon__hide');
+        this.el.classList.remove('scroll-icon__hiding');
+      }, 300);
+    }
+  },
+  addEventListener() {
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      if (this.scrolling && scrollY > 0) {
+        return;
+      }
+      this.scrolling = false;
+
+      if (scrollY > 400) {
+        this.show();
+      } else {
+        this.hide();
+      }
+    });
+    document.querySelector('.scroll-icon').onclick = () => {
+      this.scrolling = true;
+      this.hide();
+
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    };
+  },
+};
+
+btnUp.addEventListener();
