6c6cc663dbe6c4dbdd9516754044e208b5285e79
mrcolti4 <68332032+mrcolti4@users.noreply.github.com>
2023-06-29 12:36:38 +0000

    Deploying to gh-pages from @ mrcolti4/team-project-tastytreats@7dd69c82ace05e1b952f082877fb70341191b53c 🚀

--- index.fed3082c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".js-input"),t=document.querySelector(".js-clin-input"),n=document.querySelector(".js-icon-serch");e.addEventListener("focus",function(){n.classList.add("active")}),e.addEventListener("blur",function(){n.classList.remove("active")}),e.addEventListener("input",function(){if(e.value.trim().length>0){t.classList.add("active");return}t.classList.remove("active")}),t.addEventListener("click",function(){e.value="",t.classList.remove("active")})}();
-//# sourceMappingURL=index.fed3082c.js.map
