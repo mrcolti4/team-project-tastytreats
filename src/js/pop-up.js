114313ef4cd745379d82223b50f004779208f1e8
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-06-29 03:08:56 +0300

    3-08

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,11 +1,18 @@
 
-// fetchRecipeById('6462a8f74c3d0ddd28898040');
+fetchRecipeById('6462a8f74c3d0ddd28897feb');
 let refs = {
   closeVideo: document.querySelector('.tiezer-close-btn'),
   tiezer: document.querySelector('.tiezer'),
   trailerBox: document.querySelector('.trailer-box'),
-
+  btnOpenYouTube: document.querySelector('.js-btn-openYouTube'),
+  preview: document.querySelector('.recipes'),
+  video: document.querySelector('iframe'),
+  title: document.querySelector('.js-title'),
+  time: document.querySelector('.js-minute'),
 };
+let data1;
+
+
 
 const modalRecipe = document.querySelector('.js-modal-recipe');
 // /** Відкриття та закриття модального вікна */
@@ -39,9 +46,12 @@ async function fetchRecipeById(id) {
   const data = await resp.json();
   console.log(data);
 
-  renderModalRecipe(data);
-  refs = getRefs();
-
+  // renderModalRecipe(data);
+  // refs = getRefs();
+  data1 = data;
+  renderIMG(data);
+  renderVIDEO(data)
+  renderTitle(data);
 
 }
 
@@ -69,5 +79,39 @@ const stopVideos = () => {
 refs.closeVideo.addEventListener('click', stopVideos);
 
 
+// YouTUBE module
+refs.btnOpenYouTube.addEventListener('click', openPlayer);
 
+function openPlayer() {
+  refs.trailerBox.classList.add('active');
+}
 
+function renderIMG(data) {
+  refs.preview.src = data.preview;
+}
+function renderVIDEO(data) {
+
+  const markUp = `
+   <iframe
+                width="100%"
+                height="100%"
+                src="http://www.youtube.com/embed/${getKeyYouTybe(data.youtube)};
+
+title = "YouTube video player"
+frameborder = "0"
+allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+allowfullscreen
+  ></iframe > `;
+  refs.tiezer.insertAdjacentHTML('beforeend', markUp);
+}
+
+function getKeyYouTybe(url) {
+  let indexLast = url.split('').length;
+
+  let key = url.split('').splice(32, indexLast).join('');
+  return key;
+}
+
+function renderTitle(data) {
+  refs.title.textContent = data.title;
+}
