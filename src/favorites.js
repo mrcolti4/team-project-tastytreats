49ced23e0eeef359eb4c171e5be242d62d87d6c6
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-06-30 13:26:56 +0300

    fix meta, shoping card, rating, black

--- src/favorites.js
+++ src/favorites.js
@@ -1,3 +1,9 @@
 import './js/localctorage';
 import './js/header-mobile-menu';
 import './js/favorites-main';
+
+import './js/ratings';
+import './js/scheme';
+import './js/pop-up';
+import './js/order-now';
+import './js/btn-up';
